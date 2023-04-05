7423eb83ce2d087f161bc64c8b0adbebb56b153f
PiekarzM <127319430+PiekarzM@users.noreply.github.com>
2023-04-05 12:43:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+class Main {
+  public static void main(String[] args) {
+   int wysokość = 5;
+    for (int i= wysokość;i>0;i--){
+      for(int j=0;j<i;j++){
+        System.out.print("*");
+         }
+      System.out.println(" ");
+    }
+  }
+}
+     
